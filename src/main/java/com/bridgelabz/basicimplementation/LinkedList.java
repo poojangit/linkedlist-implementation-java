af30c731565faab4466d5eefaea29aeb862dac2e
BL-Poojang <pooja.ganapathi@bridgelabz.com>
2025-05-26 16:24:30 +0530

    [POOJA] INSERT-First : Implemented a logic by giving the array elements

--- src/main/java/com/bridgelabz/basicimplementation/LinkedList.java
+++ src/main/java/com/bridgelabz/basicimplementation/LinkedList.java
@@ -6,14 +6,18 @@ public class LinkedList {
 
     // method to insert the data first in the list
     public void insertFirst(int data) {
-        Node newNode = new Node(data);
-        // if there is no linked list pr if the head is null, then make the head as a new node
-        if(head == null) {
-            head = newNode;
-        }
-        else {
-            newNode.next = head;
-            head = newNode;
+        int[] arr = {10,20,30,40,50};
+        head = new Node(data);
+        for(int i=0 ; i<arr.length; i++) {
+            // if there is no linked list pr if the head is null, then make the head as a new node
+            if (head == null) {
+                head = new Node(arr[i]);
+            } else {
+                Node temp;
+                temp = new Node(arr[i]);
+                temp.next = head;
+                head = temp;
+            }
         }
     }
     // method to print the list
--- src/main/java/com/bridgelabz/basicimplementation/MainOperation.java
+++ src/main/java/com/bridgelabz/basicimplementation/MainOperation.java
@@ -7,8 +7,8 @@ public class MainOperation {
         // creating a new linked list object because the methods in the linked list classes does not belong to the class.
         LinkedList ll = new LinkedList();
         //calling the method with data as a parameters to insert the data at the beginning.
-        ll.insertFirst(20);
-        ll.insertFirst(30);
+        ll.insertFirst(5);
+//        ll.insertFirst(30);
         ll.printList();
     }
 }
